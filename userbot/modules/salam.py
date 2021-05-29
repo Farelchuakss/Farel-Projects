20335377aff02fb91e1cca39915ed677d71c9ec7
Farelchuakss <85006791+Farelchuakss@users.noreply.github.com>
2021-05-29 21:48:05 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -120,11 +120,10 @@ async def typewriter(typew):
     await typew.edit("**Gak keren lu begitu tolol, kuburan bapak lu gw gali buat dijadiin kolam renang anak paud.Cuihhhhh!!!**")
 
 
-register(outgoing=True, pattern='^.agm(?: |$)(.*)')
+@register(outgoing=True, pattern='^.agm(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**Anak Anjing Tag Gw Mulu**")
-
 
 CMD_HELP.update({
     "salam":
