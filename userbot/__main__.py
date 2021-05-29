9b584441068dded59665d512008029c93da3910c
Farelchuakss <85006791+Farelchuakss@users.noreply.github.com>
2021-05-30 00:07:50 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"RAM-UBOT ㊙️ V7.0 [SUDAH AKTIF YA ASU!]")
+    f"Farel-Projects 🔥 V0.9 [SUDAH AKTIF YA OM!]")
 
 
 if len(argv) not in (1, 3, 4):
